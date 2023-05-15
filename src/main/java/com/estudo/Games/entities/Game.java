80f8f5a1e392c40696f103faf186189f950d91a3
afrodrigues <ana.rodrigues@vr.com.br>
2023-05-15 17:55:20 -0300

    Arquivos Properties, entidade Game, ORM

---
+++ src/main/java/com/estudo/Games/entities/Game.java
@@ -0,0 +1,34 @@
+package com.estudo.Games.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_game")
+@Getter
+@Setter
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
+    private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
+    private String longDescription;
+
+
+}
+
+
